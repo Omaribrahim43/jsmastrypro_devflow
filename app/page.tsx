5a914702e0d96781d0790e42ae0ca28f45d05935
Omaribrahim43 <omar.migdadi43@gmail.com>
2025-09-10 20:33:37 +0300

    install shadcn and dropdown menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 const Home = () => {
   return (
     <>
-      <h1 className="h1-bold font-space-grotesk">Welcome to Next.js</h1>
       <h1 className="h1-bold font-inter">Welcome to Next.js</h1>
     </>
   );
