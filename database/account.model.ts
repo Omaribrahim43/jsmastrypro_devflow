18985043cf208c559ad2fba3483fc161450bd55d
Omaribrahim43 <omar.migdadi43@gmail.com>
2025-09-12 17:14:09 +0300

    implement account model

---
+++ database/account.model.ts
@@ -0,0 +1,27 @@
+import { model, models, Schema } from "mongoose";
+import { string } from "zod";
+
+export interface IAccount {
+  userId: Types.ObjectId;
+  name: string;
+  image?: string;
+  password?: string;
+  provider: string;
+  providerAccountId: string;
+}
+
+const AccountSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    name: { type: string, required: true },
+    image: { type: string },
+    password: { type: string },
+    provider: { type: string, required: true },
+    providerAccountId: { type: string, required: true },
+  },
+  { timestamps: true }
+);
+
+const Account = models?.account || model<IAccount>("Account", AccountSchema);
+
+export default Account;
