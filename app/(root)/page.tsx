a066ea78192d5b3035a6c2504e6fd4d04ba40998
Omaribrahim43 <omar.migdadi43@gmail.com>
2025-09-12 14:14:41 +0300

    fixing bugs

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,35 +1,97 @@
 import Link from "next/link";
-import React from "react";
 
-import { Badge } from "@/components/ui/badge";
+import QuestionCard from "@/components/cards/QuestionsCard";
+import HomeFilter from "@/components/filters/HomeFilter";
+import LocalSearch from "@/components/search/LocalSearch";
+import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
-import { getDeviconClassName } from "@/lib/utils";
 
-interface Props {
-  _id: string;
-  name: string;
-  questions?: number;
-  showCount?: boolean;
-  compact?: boolean;
+const questions = [
+  {
+    _id: "1",
+    title: "How to learn React?",
+    description: "I want to learn React, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: {
+      _id: "1",
+      name: "John Doe",
+      image:
+        "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
+    },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+  {
+    _id: "2",
+    title: "How to learn JavaScript?",
+    description: "I want to learn JavaScript, can anyone help me?",
+    tags: [
+      { _id: "1", name: "JavaScript" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: {
+      _id: "1",
+      name: "John Doe",
+      image:
+        "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
+    },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date("2021-09-01"),
+  },
+];
+
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
 }
 
-const TagCard = ({ _id, name, questions, showCount, compact }: Props) => {
-  const iconClass = getDeviconClassName(name);
+const Home = async ({ searchParams }: SearchParams) => {
+  const { query = "", filter = "" } = await searchParams;
+
+  const filteredQuestions = questions.filter((question) => {
+    const matchesQuery = question.title
+      .toLowerCase()
+      .includes(query.toLowerCase());
+    const matchesFilter = filter
+      ? question.tags[0].name.toLowerCase() === filter.toLowerCase()
+      : true;
+    return matchesQuery && matchesFilter;
+  });
 
   return (
-    <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
-      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
-        <div className="flex-center space-x-2">
-          <i className={`${iconClass} text-sm`}></i>
-          <span>{name}</span>
-        </div>
-      </Badge>
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
 
-      {showCount && (
-        <p className="small-medium text-dark500_light700">{questions}</p>
-      )}
-    </Link>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
+      </section>
+      <HomeFilter />
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {filteredQuestions.map((question) => (
+          <QuestionCard key={question._id} question={question} />
+        ))}
+      </div>
+    </>
   );
 };
 
-export default TagCard;
+export default Home;
