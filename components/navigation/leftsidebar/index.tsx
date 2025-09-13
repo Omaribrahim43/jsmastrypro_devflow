4bf3c290989f450c3f6a5e779286eb19d5498ab0
Omaribrahim43 <omar.migdadi43@gmail.com>
2025-09-13 17:16:36 +0300

    Implement Log Out and Show User Info on Home

--- components/navigation/leftsidebar/index.tsx
+++ components/navigation/leftsidebar/index.tsx
@@ -1,49 +1,76 @@
+import { LogOutIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+import { auth, signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 
 import NavLinks from "../navbar/NavLinks";
 
-const LeftSidebar = () => {
+const LeftSidebar = async () => {
+  const session = await auth();
+  const userId = session?.user?.id;
   return (
     <section className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 h-screen flex flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
       <div className="flex flex-1 flex-col gap-6">
-        <NavLinks />
+        <NavLinks userId={userId} />
       </div>
       <div className="flex flex-col gap-3">
-        <Button
-          asChild
-          className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-6 shadow-none"
-        >
-          <Link href={ROUTES.SIGN_IN}>
-            <Image
-              src="/icons/account.svg"
-              alt="Account"
-              width={20}
-              height={20}
-              className="invert-colors lg:hidden"
-            />
-            <span className="primary-text-gradient max-lg:hidden">Log In</span>
-          </Link>
-        </Button>
-        <Button
-          asChild
-          className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-6 shadow-none"
-        >
-          <Link href={ROUTES.SIGN_UP}>
-            <Image
-              src="/icons/sign-up.svg"
-              alt="Account"
-              width={20}
-              height={20}
-              className="invert-colors lg:hidden"
-            />
-            <span>Sign Up</span>
-          </Link>
-        </Button>
+        {userId ? (
+          <form
+            action={async () => {
+              "use server";
+              await signOut();
+            }}
+          >
+            <Button
+              type="submit"
+              className="base-medium w-fit !bg-transparent px-4 py-6"
+            >
+              <LogOutIcon className="size-5 text-black dark:text-white" />
+              <span className="max-lg:hidden text-dark300_light900">
+                Logout
+              </span>
+            </Button>
+          </form>
+        ) : (
+          <>
+            <Button
+              asChild
+              className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-6 shadow-none"
+            >
+              <Link href={ROUTES.SIGN_IN}>
+                <Image
+                  src="/icons/account.svg"
+                  alt="Account"
+                  width={20}
+                  height={20}
+                  className="invert-colors lg:hidden"
+                />
+                <span className="primary-text-gradient max-lg:hidden">
+                  Log In
+                </span>
+              </Link>
+            </Button>
+            <Button
+              asChild
+              className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-6 shadow-none"
+            >
+              <Link href={ROUTES.SIGN_UP}>
+                <Image
+                  src="/icons/sign-up.svg"
+                  alt="Account"
+                  width={20}
+                  height={20}
+                  className="invert-colors lg:hidden"
+                />
+                <span>Sign Up</span>
+              </Link>
+            </Button>
+          </>
+        )}
       </div>
     </section>
   );
