55aebefb164311732397337162c19a0cd498ad6f
omaribrahim43 <omar.migdadi43@gmail.com>
2025-09-24 14:32:56 +0300

    Implement Render UI States

--- constants/routes.ts
+++ constants/routes.ts
@@ -3,8 +3,12 @@ const ROUTES = {
   SIGN_IN: "/sign-in",
   SIGN_UP: "/sign-up",
   ASK_QUESTION: "/ask-question",
+  COLLECTION: "/collection",
+  COMMUNITY: "/cummunity",
+  TAGS: "/tags",
+  JOBS: "/jobs",
   PROFILE: (id: string) => `/profile/${id}`,
-  TAGS: (id: string) => `/tags/${id}`,
+  TAG: (id: string) => `/tags/${id}`,
   QUESTION: (id: string) => `/questions/${id}`,
   SIGN_IN_WITH_OAUTH: `signin-with-oauth`,
 };
